75990924be8ec2f6156dcbc733b53fd42c7d2864
Karen Peñate <kpenate@salud.gob.sv>
2013-04-17 11:13:14 -0600

    Cambiando Plantilla

---
+++ src/Minsal/SiapsBundle/Admin/CtlParentescoAdmin.php
@@ -0,0 +1,16 @@
+<?php
+namespace Minsal\SiapsBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+
+class CtlParentescoAdmin extends Admin
+{
+    
+}
+
+
+?>
