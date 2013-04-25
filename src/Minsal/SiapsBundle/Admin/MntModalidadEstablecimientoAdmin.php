5e62aa7801cd3b57895f46bf5fb06ff9d8b358c9
Victoria López <mvlopez@salud.gob.sv>
2013-04-25 15:30:44 -0600

    cambiando estilos layout

--- src/Minsal/SiapsBundle/Admin/MntModalidadEstablecimientoAdmin.php
+++ src/Minsal/SiapsBundle/Admin/MntModalidadEstablecimientoAdmin.php
@@ -9,6 +9,12 @@ use Sonata\AdminBundle\Validator\ErrorElement;
 
 class MntModalidadEstablecimientoAdmin extends Admin
 {
+    protected $datagridValues = array(
+        '_page' => 1, // Display the first page (default = 1)
+        '_sort_order' => 'ASC', // Descendant ordering (default = 'ASC')
+        '_sort_by' => 'idEstablecimiento' // name of the ordered field (default = the model id field, if any)
+    );
+    
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
@@ -31,7 +37,7 @@ class MntModalidadEstablecimientoAdmin extends Admin
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
-            ->add('idEstablecimiento',null, array('label'=> $this->getTranslator()->trans('establecimiento')))
+            ->addIdentifier('idEstablecimiento',null, array('label'=> $this->getTranslator()->trans('establecimiento')))
             ->add('idModalidad', null, array('label'=>$this->getTranslator()->trans('id_modalidad')))
             ->add('tieneBodega', null, array('label'=>'Tiene bodega para farmacia'))
             ->add('repetitiva', null , array('label'=>'Emite recetas repetitivas'))
