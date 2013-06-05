915d6cb4d20821a7c3e6118cc949143477f59be2
Karen Peñate <kpenate@salud.gob.sv>
2013-06-05 13:56:12 -0600

    Realizando cambios a la busqueda de paciente.

--- src/Minsal/SiapsBundle/Resources/public/js/MntPacienteAdmin/jqGridBusqueda.js
+++ src/Minsal/SiapsBundle/Resources/public/js/MntPacienteAdmin/jqGridBusqueda.js
@@ -9,9 +9,9 @@ $(document).ready(function() {
         colNames: ['idPaciente', 'NEC','Apellidos','Nombres', 'F. NAC.', 'DUI', 'Nombre Madre', 'Conocido por'],
         colModel: [
             {name: 'id', index: 'id', editable: false, editoptions: {size: 15}},
-            {name: 'nec', index: 'nec', editable: false, width: 50},
-            {name: 'apellidos', index: 'apellidos', editable: false, width: 250},
-            {name: 'nombres', index: 'nombres', editable: false, width: 250},
+            {name: 'nec', index: 'nec', editable: false, width: 80},
+            {name: 'apellidos', index: 'apellidos', editable: false, width: 200},
+            {name: 'nombres', index: 'nombres', editable: false, width: 200},
             {name: 'fecha_nacimiento', index: 'fecha_nacimiento', editable: false, width: 80},
             {name: 'dui', index: 'dui', editable: false, width: 80},
             {name: 'nombre_madre', index: 'nombre_madre', editable: false, width: 250},
