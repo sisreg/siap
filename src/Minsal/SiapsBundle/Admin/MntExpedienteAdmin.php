e99b04fc01b9ec2a83ea9cbfa6a11c5105b4d5fb
Karen Peñate <kpenate@salud.gob.sv>
2013-06-19 12:08:09 -0600

    Agregando pestaña de reporte de expedientes creados

--- src/Minsal/SiapsBundle/Admin/MntExpedienteAdmin.php
+++ src/Minsal/SiapsBundle/Admin/MntExpedienteAdmin.php
@@ -4,26 +4,32 @@ namespace Minsal\SiapsBundle\Admin;
 
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\AdminBundle\Route\RouteCollection;
-use Sonata\AdminBundle\Validator\ErrorElement;
+
 
 class MntExpedienteAdmin extends Admin {
 
     protected function configureFormFields(FormMapper $formMapper) {
-        $esomed=$this->getModelManager()
-                ->findOneBy('MinsalSiapsBundle:CtlCreacionExpediente', array('id'=>3));
-                
+        $esomed = $this->getModelManager()
+                ->findOneBy('MinsalSiapsBundle:CtlCreacionExpediente', array('id' => 3));
         $formMapper
-                ->add('numero',null,array('required'=>true,'label'=>$this->getTranslator()->trans('numero')))
-                ->add('idCreacionExpediente',null,array('required'=>true,
+                ->add('numero', null, array('required' => true, 'label' => $this->getTranslator()->trans('numero')))
+                ->add('idCreacionExpediente', null, array('required' => true,
                     'preferred_choices' => array($esomed),
-                    'label'=>$this->getTranslator()->trans('idCreacionExpediente')))
-                ->add('idPaciente',null,array('label'=>$this->getTranslator()->trans('idPaciente')))
+                    'label' => $this->getTranslator()->trans('idCreacionExpediente')))
+                ->add('idPaciente', null, array('label' => $this->getTranslator()->trans('idPaciente')))
         ;
     }
-
-   protected function configureRoutes(RouteCollection $collection) {
-        $collection->remove('list');
+     public function getTemplate($name) {
+        switch ($name) {
+            case 'list':
+                return 'MinsalSiapsBundle:MntExpedienteAdmin:list.html.twig';
+                break;
+            default:
+                return parent::getTemplate($name);
+                break;
+        }
     }
+
 }
+
 ?>
---
+++ src/Minsal/SiapsBundle/Controller/MntExpedienteAdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Minsal\SiapsBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+
+class MntExpedienteAdminController extends Controller {
+
+    public function listAction() {
+        if (false === $this->admin->isGranted('LIST')) {
+            return $this->render('MinsalSiapsBundle::Accesso_denegado.html.twig', array('admin_pool' => $this->container->get('sonata.admin.pool'),
+                        'layout' => $this->container->get('sonata.admin.pool')->getTemplate('layout')
+            ));
+        }
+        $em = $this->getDoctrine()->getManager();
+        $establecimiento = $em->getRepository("MinsalSiapsBundle:CtlEstablecimiento")->obtenerEstablecimientoConfigurado();
+        
+        return $this->render($this->admin->getTemplate('list'), array(
+                    'action' => 'list',
+                    'establecimiento' => $establecimiento
+        ));
+    }
+
+}
