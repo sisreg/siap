45f92782a70cdb470f260a6fcac0e62a2aa1afb4
Karen Peñate <kpenate@salud.gob.sv>
2013-07-16 09:10:24 -0600

    Realizando cambios a la plantilla de paciente

--- src/Minsal/SiapsBundle/Controller/MntPacienteAdminController.php
+++ src/Minsal/SiapsBundle/Controller/MntPacienteAdminController.php
@@ -175,26 +175,11 @@ class MntPacienteAdminController extends Controller {
     }
 
     public function redirectTo($object) {
-        $url = false;
 
-        if ($this->get('request')->get('btn_update_and_list')) {
-            $url = $this->admin->generateUrl('list');
-        }
-        if ($this->get('request')->get('btn_create_and_list')) {
-            $url = $this->admin->generateUrl('list');
-        }
-        if ($this->get('request')->get('btn_create_and_create')) {
-            $params = array();
-            if ($this->admin->hasActiveSubClass()) {
-                $params['subclass'] = $this->get('request')->get('subclass');
-            }
-            $url = $this->admin->generateUrl('create', $params);
-        }
-        if (!$url) {
-            $params = array();
-            $params['id'] = $object->getId();
-            $url = $this->admin->generateUrl('view', $params);
-        }
+
+        $params = array();
+        $params['id'] = $object->getId();
+        $url = $this->admin->generateUrl('view', $params);
 
         return new RedirectResponse($url);
     }
