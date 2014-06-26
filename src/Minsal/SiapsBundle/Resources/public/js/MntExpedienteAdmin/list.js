91104aff071d60c38d1059ae88c91cfdfedc43ef
Karen Elvira Peñate <kpenate@salud.gob.sv>
2014-06-26 10:58:42 -0600

    Agregando validaciones faltantes

--- src/Minsal/SiapsBundle/Resources/public/js/MntExpedienteAdmin/list.js
+++ src/Minsal/SiapsBundle/Resources/public/js/MntExpedienteAdmin/list.js
@@ -1,13 +1,43 @@
 $(document).ready(function() {
     $("#fecha_inicio").datepicker().mask("99-99-9999");
     $("#fecha_fin").datepicker().mask("99-99-9999");
-    
+
     $("#emitir_informe").click(function() {
-        $('#resultado').load(Routing.generate('expedientes_creados'),{'datos':$('#expedientes_creados').serialize()});
+       
+        if ($("#fecha_inicio").val() == '' || $("#fecha_fin").val() == '') {
+            ($('#error')) ? $('#error').remove() : '';
+            var elem = $("<div id='error' title='Error de llenado'><center>" +
+                    "Debe de seleccionar ambas fechas para generar el reporte."
+                    + "</center></div>");
+            elem.insertAfter($("#expedientes_creados"));
+            $("#error").dialog({
+                close: function() {
+                    if ($("#fecha_inicio").val() == '')
+                        $("#fecha_inicio").focus();
+                    else
+                        $("#fecha_fin").focus();
+                }
+            });
+        } else if ($("#fecha_inicio").datepicker("getDate") > $("#fecha_fin").datepicker("getDate")) {
+            ($('#error')) ? $('#error').remove() : '';
+            var elem = $("<div id='error' title='Error de llenado'><center>" +
+                    "La fecha de inicio debe de ser menor que la fecha fin."
+                    + "</center></div>");
+            elem.insertAfter($("#expedientes_creados"));
+            $("#error").dialog({
+                close: function() {
+                    $("#fecha_inicio").val('');
+                    $("#fecha_fin").val('');
+                    $("#fecha_inicio").focus();
+                }
+            });
+        } else {
+            $('#resultado').load(Routing.generate('expedientes_creados'), {'datos': $('#expedientes_creados').serialize()});
+        }
         return false;
     });
-   
-    
+
+
 //para exportar reportes
     $('#exportar_hoja_calculo').click(function() {
         if ($('.ui-paging-info').text() != 'Sin registros que mostrar') {
@@ -15,10 +45,10 @@ $(document).ready(function() {
             window.open(url, '_blank');
             return false;
         }
-       else {
+        else {
             return false;
         }
-        
+
     });
     $('#exportar_pdf').click(function() {
         if ($('.ui-paging-info').text() != 'Sin registros que mostrar') {
@@ -29,7 +59,7 @@ $(document).ready(function() {
         else {
             return false;
         }
-        
+
     });
 });
 
